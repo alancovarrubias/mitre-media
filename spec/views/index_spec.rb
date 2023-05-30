7463343469f0604b6384d5e77bf5447bdfee2979
Alan Covarrubias <alan.cov@icloud.com>
2023-05-30 15:12:21 -0700

    add links to show action

--- spec/views/index_spec.rb
+++ spec/views/index_spec.rb
@@ -1,13 +1,19 @@
 RSpec.describe 'sentences/index.html.erb', type: :view do
   it "displays the sentence's text" do
-    sentence1 = Sentence.create(text: 'This is sentence 1.')
-    sentence2 = Sentence.create(text: 'This is sentence 2.')
-
-    assign(:sentences, [sentence1, sentence2])
+    assign(:sentences, [])
 
     render
 
-    expect(rendered).to have_content('This is sentence 1.')
-    expect(rendered).to have_content('This is sentence 2.')
+    expect(rendered).to have_content('Sentences')
+  end
+
+  it 'redirects to the show action when a resource link is clicked' do
+    sentence = Sentence.create(text: 'This is a sentence.')
+
+    assign(:sentences, [sentence])
+
+    render
+
+    expect(rendered).to have_link('This is a sentence.', href: sentence_path(sentence))
   end
 end
