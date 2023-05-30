5f111b2d451c56a66c415e18284e07e3de8b3029
Alan Covarrubias <alan.cov@icloud.com>
2023-05-30 15:32:21 -0700

    add show action to sentences controller

--- spec/controllers/sentences_controller_spec.rb
+++ spec/controllers/sentences_controller_spec.rb
@@ -1,8 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe SentencesController, type: :controller do
-  it 'renders the index template' do
-    get :index
-    expect(response).to render_template('index')
+  describe 'GET index' do
+    it 'assigns the correct sentence to @sentence' do
+      sentence = Sentence.create(text: 'This is a sentence.')
+
+      get :index
+
+      expect(assigns(:sentences)).to eq([sentence])
+    end
+
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET show' do
+    it 'assigns the correct sentence to @sentence' do
+      sentence = Sentence.create(text: 'This is a sentence.')
+
+      get :show, params: { id: sentence.id }
+
+      expect(assigns(:sentence)).to eq(sentence)
+    end
+
+    it 'renders the show template' do
+      sentence = Sentence.create(text: 'This is a sentence.')
+
+      get :show, params: { id: sentence.id }
+
+      expect(response).to render_template(:show)
+    end
   end
 end
